c7f2986ddc0b06fd654c3cb9364cab362070f3ad
afmjoaa <mohimenul.joaa@bkash.com>
2023-04-21 11:22:30 +0600

    SPY-#3: Format code

--- spacy/src/utility.py
+++ spacy/src/utility.py
@@ -1,10 +1,10 @@
 class Constants:
-    SEPERATOR = ' _ _ '
-    ID_IDENTIFIER = '#'
-    BIOX_SEPERATOR = '-'
-    BEGEN = 'B'
-    INSIDE = 'I'
-    OUTSIDE = 'O'
+    SEPERATOR = " _ _ "
+    ID_IDENTIFIER = "#"
+    BIOX_SEPERATOR = "-"
+    BEGEN = "B"
+    INSIDE = "I"
+    OUTSIDE = "O"
     BEGEN_ONEHOT = [1, 0, 0]
     INSIDE_ONEHOT = [0, 1, 0]
     OUTSIDE_ONEHOT = [0, 0, 1]
